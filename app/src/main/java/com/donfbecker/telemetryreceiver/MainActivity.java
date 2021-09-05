bad9670ce76861bccc760621a3105eabf4bf6293
Don Becker <dbecker@exodus.psyon.org>
2021-09-04 22:37:15 -0500

    First working version

---
+++ app/src/main/java/com/donfbecker/telemetryreceiver/MainActivity.java
@@ -0,0 +1,153 @@
+package com.donfbecker.telemetryreceiver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.donfbecker.telemetryreceiver.R;
+
+public class MainActivity extends AppCompatActivity {
+    private RtlSdrStreamer streamer;
+
+    private int currentFrequency = 148500000;
+    private int currentGain = 0;
+    private int currentSquelch = 0;
+
+    private TextView textFrequency;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        streamer = new RtlSdrStreamer();
+
+        final Button startButton = findViewById(R.id.button_start);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d("DEBUG", "iqsrc://-a 127.0.0.1 -p 1234 -s " + RtlSdrStreamer.IQ_SAMPLE_RATE);
+                Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("iqsrc://-a 127.0.0.1 -p 1234 -g 0 -f 148420440 -s " + RtlSdrStreamer.IQ_SAMPLE_RATE));
+                startActivityForResult(intent, 1234);
+            }
+        });
+
+        final Button stopButton = findViewById(R.id.button_stop);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                streamer.stop();
+            }
+        });
+
+        final SeekBar gainSeekBar = findViewById(R.id.seek_gain);
+        gainSeekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int gain, boolean b) {
+                        setGain(gain);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) { }
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) { }
+                }
+        );
+
+        final SeekBar squelchSeekBar = findViewById(R.id.seek_squelch);
+        squelchSeekBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int squelch, boolean b) {
+                        setSquelch(squelch);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) { }
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) { }
+                }
+        );
+
+        textFrequency = findViewById(R.id.text_frequency);
+        textFrequency.setText(String.format("%.6f", (currentFrequency / 1000000f)));
+    }
+
+    public void onIncreaseFrequency(View v) {
+        int n = Integer.parseInt(getResources().getResourceEntryName(v.getId()).replace("button_plus_", ""));
+        setFrequency(currentFrequency + n);
+    }
+
+    public void onDecreaseFrequency(View v) {
+        int n = Integer.parseInt(getResources().getResourceEntryName(v.getId()).replace("button_minus_", ""));
+        setFrequency(currentFrequency - n);
+    }
+
+    public void onSetFrequency(View v) {
+        int n = Integer.parseInt(getResources().getResourceEntryName(v.getId()).replace("button_frequency_", ""));
+        setFrequency(n);
+    }
+
+    private void setFrequency(int frequency) {
+        this.currentFrequency = frequency;
+        textFrequency.setText(String.format("%.6f", (currentFrequency/1000000.0)));
+        streamer.setFrequency(frequency);
+    }
+
+    private void setGain(int gain) {
+        this.currentGain = gain;
+        streamer.setGain(gain);
+    }
+
+    private void setSquelch(int squelch) {
+        this.currentSquelch = squelch;
+        streamer.setSquelch(squelch);
+    }
+
+    public void onGainChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Log.d("DEBUG", "Got progress change of: " + progress);
+    }
+
+    public void onSquelchChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        Log.d("DEBUG", "Got progress change of: " + progress);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        streamer.stop();
+
+        // Invalid arguments cause the driver to stop
+        Intent intent = new Intent(Intent.ACTION_VIEW).setData(Uri.parse("iqsrc://-x"));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("DEBUG", "onActivityResult(" + requestCode + ", " + resultCode + ")");
+
+        if (requestCode != 1234) return; // This is the requestCode that was used with startActivityForResult
+        if (resultCode == RESULT_OK) {
+            // Connection with device has been opened and the rtl-tcp server is running. You are now responsible for connecting.
+            int[] supportedTcpCommands = data.getIntArrayExtra("supportedTcpCommands");
+            streamer.start();
+            streamer.setFrequency(currentFrequency);
+            streamer.setGain(currentGain);
+        } else {
+            // something went wrong, and the driver failed to start
+            String message = data.getStringExtra("detailed_exception_message");
+            Log.d("ERROR", message); // Show the user why something went wrong
+        }
+    }
+}
---
+++ app/src/main/java/com/donfbecker/telemetryreceiver/RtlSdrStreamer.java
@@ -0,0 +1,169 @@
+package com.donfbecker.telemetryreceiver;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.lang.Thread;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
+import android.util.Log;
+
+public class RtlSdrStreamer {
+    public static final int IQ_SAMPLE_RATE = 1920000;
+    public static final int AUDIO_SAMPLE_RATE = 48000;
+
+    public static final byte COMMAND_SET_FREQUENCY 	= 0x01;
+    public static final byte COMMAND_SET_SAMPLERATE = 0x02;
+    public static final byte COMMAND_SET_GAIN_MODE	= 0x03;
+    public static final byte COMMAND_SET_GAIN 		= 0x04;
+    public static final byte COMMAND_SET_FREQ_CORR 	= 0x05;
+    public static final byte COMMAND_SET_IFGAIN 	= 0x06;
+    public static final byte COMMAND_SET_AGC_MODE 	= 0x08;
+
+    private Socket connection;
+    private InputStream stream;
+
+    private boolean stayAlive = false;
+    private AudioTrack audioTrack;
+    private int trackBufferSize;
+    private int iqBufferSize;
+
+    private float squelch = 0;
+
+    private ArrayBlockingQueue<byte[]> commandQueue = null;
+
+    public RtlSdrStreamer() {
+        commandQueue = new ArrayBlockingQueue<byte[]>(100);
+    }
+
+    public boolean isRunning() {
+        return false;
+    }
+
+    public boolean start() {
+        Log.d("DEBUG", "RtlSdrStreamer.start()");
+
+        trackBufferSize = AudioTrack.getMinBufferSize(AUDIO_SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_8BIT);
+        iqBufferSize = trackBufferSize * 40 * 2;
+
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, AUDIO_SAMPLE_RATE, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_8BIT, trackBufferSize, AudioTrack.MODE_STREAM);
+
+        new Thread() {
+            byte[] trackBuffer = new byte[trackBufferSize];
+            byte[] iqBuffer = new byte[iqBufferSize];
+
+            public void run() {
+                try {
+                    connection = new Socket("127.0.0.1", 1234);
+                    stream = connection.getInputStream();
+                    DataInputStream input = new DataInputStream(stream);
+
+                    // Let's try disabling AGC
+                    setAGCMode(false);
+                    setGainMode(true);
+                    //setGain(1200);
+
+                    audioTrack.play();
+                    stayAlive = true;
+                    while (stayAlive) {
+                        input.readFully(iqBuffer);
+
+                        for (int j = 0; j < trackBufferSize; j++) {
+                            double sumI = 0;
+                            double sumQ = 0;
+                            for (int k = 0; k < 40; k++) {
+                                sumI += (double) (((iqBuffer[(j * 80) + (k * 2)] & 0xFF) - 127.5) / 127.5);
+                                sumQ += (double) (((iqBuffer[(j * 80) + (k * 2) + 1] & 0xFF) - 127.5) / 127.5);
+                            }
+
+                            double i = sumI / 40;
+                            double q = sumQ / 40;
+                            double a = (i * i) + (q * q);
+
+                            byte v = (byte) (127 + ((i + q) * 127));
+                            if(a < squelch) v = 127;
+                            //if(Math.abs(v - 128) < 10) v = 127;
+                            trackBuffer[j] = v;
+                        }
+
+                        audioTrack.write(trackBuffer, 0, trackBufferSize);
+
+                        // Check command queue for packets
+                        byte[] packet = commandQueue.poll();
+                        if (packet != null) connection.getOutputStream().write(packet);
+                    }
+
+                    audioTrack.stop();
+                    connection.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+        return true;
+    }
+
+    public boolean stop() {
+        stayAlive = false;
+        return true;
+    }
+
+    public boolean setFrequency(int frequency) {
+        return sendCommand(COMMAND_SET_FREQUENCY, frequency);
+    }
+
+    public boolean setGainMode(boolean manual) {
+        return sendCommand(COMMAND_SET_GAIN_MODE, manual ? 0x01 : 0x00);
+    }
+
+    public boolean setGain(int gain) {
+        return sendCommand(COMMAND_SET_GAIN, gain);
+    }
+
+    public boolean setSquelch(int squelch) {
+        this.squelch = (float)squelch / 10000.0f;
+        Log.d("DEBUG", "Squelch is " + this.squelch);
+        return true;
+    }
+
+    public boolean setAGCMode(boolean enabled) {
+        return sendCommand(COMMAND_SET_AGC_MODE, enabled ? 0x01 : 0x00);
+    }
+
+    private boolean sendCommand(byte command, int arg) {
+        if(connection == null || !connection.isConnected()) return false;
+        commandQueue.offer(commandToPacket(command, arg));
+        return true;
+    }
+
+    private boolean sendCommand(byte command, short arg1, short arg2) {
+        if(connection == null || !connection.isConnected()) return false;
+        commandQueue.offer(commandToPacket(command, arg1, arg2));
+        return true;
+    }
+    private byte[] commandToPacket(byte command, int arg) {
+        byte[] packet = new byte[5];
+        packet[0] = command;
+        packet[1] = (byte)((arg >> 24) & 0xFF);
+        packet[2] = (byte)((arg >> 16) & 0xFF);
+        packet[3] = (byte)((arg >> 8) & 0xFF);
+        packet[4] = (byte)(arg & 0xFF);
+
+        return packet;
+    }
+
+    private byte[] commandToPacket(byte command, short arg1, short arg2) {
+        byte[] packet = new byte[5];
+        packet[0] = command;
+        packet[1] = (byte)((arg1 >> 8) & 0xFF);
+        packet[2] = (byte)(arg1 & 0xFF);
+        packet[3] = (byte)((arg2 >> 8) & 0xFF);
+        packet[4] = (byte)(arg2 & 0xFF);
+
+        return packet;
+    }
+}
